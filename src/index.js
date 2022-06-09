73ff06768d440ce1eec95fdebd96a358d09a63f3
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-06-09 19:34:16 +0200

    Nested root

--- src/index.js
+++ src/index.js
@@ -12,6 +12,23 @@ function Baz() {
   return <h1>Baz</h1>
 }
 
+function Form() {
+  return (
+    <form action="#">
+      <label htmlFor="username">
+        Username:
+        <input type="text" id="username" />
+      </label>
+      <label htmlFor="" id="password">
+        Password:
+        <input type="password" id="password" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
 createRoot(document.getElementById('foo')).render(<Foo />);
 createRoot(document.getElementById('bar')).render(<Bar />);
 createRoot(document.getElementById('baz')).render(<Baz />);
+createRoot(document.getElementById('nested-root')).render(<Form />);
