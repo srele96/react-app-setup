d5ceedf99376ec153a383d4d6955b7a6e9afcb0c
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-06-09 20:51:42 +0200

    Use css module

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <article>
+      <h1>React App Setup</h1>
+      <button className={styles.btn}>Button</button>
+    </article>
+  )
+}
+
+export { App };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { createRoot } from 'react-dom/client';
+import { App }from './App';
 import './index.css';
-
-function App() {
-  return <h1>React App Setup</h1>
-}
 
 createRoot(document.getElementById('root')).render(<App />);
